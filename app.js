16e38a6ce37c32fe3fdddaebdcd5b501b4aca7d2
Bitlab234 <balabanovvovik@mail.ru>
2024-11-26 19:38:42 +0400

    Настроены маршруты в шаблоне шапки, и часть в шаюлоне подвала

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3100;
 const indexRouter = require('./routes/index');
+const trainersRouter = require('./routes/trainers');
+const contactsRouter = require('./routes/contacts');
 
 // Настройка EJS в качестве шаблонизатора
 app.set('view engine', 'ejs');
@@ -15,6 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // Подключение маршрутов
 app.use('/', indexRouter);
+app.use('/trainers', trainersRouter);
+app.use('/contacts', contactsRouter);
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,7 @@
 const callRequestModel = require('../models/call_requests_model');
 const crypto = require('crypto');
 
+//защита от xss атак
 const getIndexPage = async (req, res) => {
     try {
       const nonce = crypto.randomBytes(16).toString('hex');
@@ -9,9 +10,29 @@ const getIndexPage = async (req, res) => {
       console.error(error);
       res.status(500).send('Ошибка сервера');
     }
-  };
+};
 
-  const createCallRequest = async (req, res) => {
+const getTrainersPage = async (req, res) => {
+    try {
+      const nonce = crypto.randomBytes(16).toString('hex');
+      res.render('trainers', { nonce });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Ошибка сервера');
+    }
+};
+
+const getContactsPage = async (req, res) => {
+    try {
+      const nonce = crypto.randomBytes(16).toString('hex');
+      res.render('contacts', { nonce });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Ошибка сервера');
+    }
+};
+
+const createCallRequest = async (req, res) => {
     try {
         const { fio, phone, comment } = req.body;
         await callRequestModel.createCallRequest({ full_name: fio, phone, comment });
@@ -24,5 +45,7 @@ const getIndexPage = async (req, res) => {
 
 module.exports = {
     getIndexPage,
+    getTrainersPage,
+    getContactsPage,
     createCallRequest,
 };
